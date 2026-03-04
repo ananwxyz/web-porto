f47dcd1ffddc158e7fbb57bbc005f48466c07e04
user1 <user1@users.noreply.github.com>
2026-03-04 23:18:19 +0700

    fix: remove base path in vite config for root deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/web-porto/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
